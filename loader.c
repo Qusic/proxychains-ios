e528d0fadf3ca50b53fad8a55b752582fdd5624c
Qusic <hi@qusic.me>
2015-11-30 15:39:00 +0800

    trivial changes

--- loader.c
+++ loader.c
@@ -3,15 +3,16 @@
 
 __attribute__((constructor))
 static void constructor(void) {
-    void *handle = dlopen(LIB_DIR "/" DLL_NAME, RTLD_LAZY | RTLD_LOCAL | RTLD_FIRST);
-#define hook(symbol) MSHookFunction(dlsym(RTLD_DEFAULT, #symbol), dlsym(handle, #symbol), (void **)dlsym(handle, "true_" #symbol))
-    hook(connect);
-    hook(sendto);
-    hook(gethostbyname);
-    hook(getaddrinfo);
-    hook(freeaddrinfo);
-    hook(gethostbyaddr);
-    hook(getnameinfo);
-    hook(close);
-#undef hook
+    void *proxychains = dlopen(LIB_DIR "/" DLL_NAME, RTLD_LAZY | RTLD_LOCAL | RTLD_FIRST);
+#define _hook(handle, symbol) MSHookFunction(dlsym(handle, #symbol), dlsym(proxychains, #symbol), (void **)dlsym(proxychains, "true_" #symbol))
+#define hook(handle) \
+    _hook(handle, connect); \
+    _hook(handle, sendto); \
+    _hook(handle, gethostbyname); \
+    _hook(handle, getaddrinfo); \
+    _hook(handle, freeaddrinfo); \
+    _hook(handle, gethostbyaddr); \
+    _hook(handle, getnameinfo); \
+    _hook(handle, close);
+    hook(RTLD_DEFAULT);
 }
